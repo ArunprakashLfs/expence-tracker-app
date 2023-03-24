92552226580d8f2ec1055d77ef814c30ff12d988
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-24 20:55:50 +0530

    add input from elements

---
+++ script.js
@@ -0,0 +1,14 @@
+'use script'
+//input element from user
+//form input
+let inputEl = document.getElementById("input-text");
+let amountEl = document.getElementById("amount");
+let submitEl = document.getElementById("submit");
+
+//user apply output
+let balanceEl = document.getElementById("balance");
+let incomeEl = document.getElementById("income");
+let outcomeEl = document.getElementById("outcome");
+
+//user display elements
+let listEl = document.getElementById("list");
