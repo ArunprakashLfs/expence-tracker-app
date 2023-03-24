356c2d47b5a28d78b3cf2706dc07cb32b62be62a
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-24 22:40:36 +0530

    add reduce method to calculate value

--- script.js
+++ script.js
@@ -12,3 +12,54 @@ let outcomeEl = document.getElementById("outcome");
 
 //user display elements
 let listEl = document.getElementById("list");
+
+//dummydata
+let dummydata = [
+    {id: 1, inputEl: "flower", amountEl: -200},
+    {id: 2, inputEl: "salary", amountEl: 12000},
+]
+//function
+let transactions = dummydata;
+
+function loadtransaction(transaction){
+    const sign = transaction.amountEl < 0 ? "-": "+";
+    const item = document.createElement("li");
+    console.log(item)
+    item.classList.add(transaction.amountEl < 0 ? "outcome-list": "income-list");
+    item.innerHTML = `
+    <span>${transaction.inputEl}</span>
+    <span>${sign}${Math.abs(transaction.amountEl)}</span>
+    <button onclick="removeTrans(${transaction.id})">␡</button>`
+    listEl.appendChild(item);
+}
+
+function removeTrans(id){
+    // console.log(id);
+    transactions = transactions.filter((transaction)=> transaction.id != id);
+    config();
+}
+function updateAmount(){
+    let amounts = transactions.map((transaction)=> transaction.amountEl);
+    console.log(amounts);
+    let totalBalance = amounts.reduce(((acc,val) => acc+=val),0);
+    console.log(totalBalance);
+    balanceEl.textContent = `₹${totalBalance}`;
+    
+}
+function config(){
+    listEl.innerHTML = "";
+    transactions.forEach(loadtransaction);
+    updateAmount();
+}
+
+
+//add Event listners
+window.addEventListener("load", function(){
+    config()
+})
+submitEl.addEventListener("click", function(e){
+    e.preventDefault();
+    let input = inputEl.value;
+    let amount = amountEl.value;
+    console.log(input, amount)
+})
